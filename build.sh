5ff3500fcb2075d9eb0df87a50be5371a91b0b81
zhanglongde <1039326495@qq.com>
2017-12-18 16:37:22 +0800

    加入编译文件

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!bin/sh
+
+npm run build
+
+echo $1
+echo $2
+
+git add -f dist && git commit -m "$2"
+
+if [ -z "$1" ]
+then
+  echo "Which folder do you want to deploy to GitHub Pages?"
+  exit 1
+fi
+git subtree push --prefix $1 origin gh-pages
