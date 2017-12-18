53dc3c552b14a8c4c80f35b0e1db218151c1e252
zhanglongde <1039326495@qq.com>
2017-12-18 23:29:29 +0800

    tooltip区别于poptip

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,6 @@
     <router-view/>
   </div>
 </template>
-<style scoped lang="scss">
-  /*@import "../node_modules/font-awesome/css/font-awesome.min.css";*/
-  /*@import "../node_modules/simple-line-icons/css/simple-line-icons.css";*/
-  /*@import '~font-awesome/css/font-awesome.min.css';*/
-  /*@import '~simple-line-icons/css/simple-line-icons.css';*/
-</style>
 <style lang="scss">
   @import "assets/scss/index.scss";
 </style>
